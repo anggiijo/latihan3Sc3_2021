1416e75ecdde8b70876d0bad6af779e5b06c0b33
JO <anggijohanes@gmail.com>
2021-12-29 21:26:08 +0700

    Fourth Commit

--- Hello.java
+++ Hello.java
@@ -1,5 +1,6 @@
 class Hello{
 public static void main(String[] args){
-System.out.println("JO: Aplikasi Java 2020");
+System.out.println("JO: Aplikasi Java 2021");
+System.out.println("--- *** ---");
 }
 }
