9581ee6d9fcaf368433643f0c94b618c38ffa692
FarhanAp <farhanaldisyah@gmail.com>
2022-01-03 15:01:59 +0700

    EDIT CLONE

--- Hello.java
+++ Hello.java
@@ -3,5 +3,6 @@ public static void main(String[] args){
 System.out.println("JO: Aplikasi Java 2021");
 System.out.println("--- *** ---");
 System.out.println("END");
+System.out.println("FARHAN DAN ANGGI JO");
 }
 }
